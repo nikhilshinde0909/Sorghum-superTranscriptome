1a07f0e7f519f3d675e63eba7eeab4621c306bcc
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2022-05-31 17:31:30 +0530

    Create tools.groovy

---
+++ blat_pipe/tools.groovy
@@ -0,0 +1,4 @@
+// Path to tools used by the pipeline
+bpipe="/opt/data/home/nikhil/anaconda3/bin/bpipe"
+blat="/opt/data/home/nikhil/anaconda3/bin/blat"
+pblat="/opt/data/home/nikhil/anaconda3/bin/pblat"
