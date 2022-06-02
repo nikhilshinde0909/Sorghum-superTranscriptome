4579066a35f2b5ee409ce4b5eefe9d15dbb6fb21
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2022-06-02 19:18:02 +0530

    Create Trinotate.groovy

---
+++ Trinotate_annoatation/Trinotate.groovy
@@ -0,0 +1,15 @@
+/***********************************************************
+ ** Author: Nikhil Shinde <sd1172@srmist.edu.in>
+ ** Last Update: 30/04/2022
+ *********************************************************/
+
+VERSION="1.00"
+ 
+blast_options="-max_target_seqs 1 -outfmt 6"
+signalp_options="-f short -n"
+rnammer_options="--path_to_rnammer"
+
+load args[0]
+codeBase = file(bpipe.Config.config.script).parentFile.absolutePath
+load codeBase+"/tools.groovy"
+load codeBase+"/blatpipe_stages/blat.groovy"
