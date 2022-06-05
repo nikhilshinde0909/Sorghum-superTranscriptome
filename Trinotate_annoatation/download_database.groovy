800b893b8c5835d6ffc653c143418d9f93dabcbf
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2022-06-05 13:47:24 +0530

    Create download_database.groovy

---
+++ Trinotate_annoatation/download_database.groovy
@@ -0,0 +1,11 @@
+/***********************************************************
+ ** Author: Nikhil Shinde <sd1172@srmist.edu.in>
+ ** Last Update: 30/04/2022
+ *********************************************************/
+
+VERSION="1.00"
+
+load args[0]
+codeBase = file(bpipe.Config.config.script).parentFile.absolutePath
+load codeBase+"/tools.groovy"
+load codeBase+"/trinotate_stages/"
