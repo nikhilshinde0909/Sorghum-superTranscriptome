1b91f3bbfe4540da7f3bb11d63310c0321cdc599
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2022-06-05 15:14:32 +0530

    Update download_database.groovy

--- Trinotate_annoatation/download_database.groovy
+++ Trinotate_annoatation/download_database.groovy
@@ -8,7 +8,33 @@ VERSION="1.00"
 load args[0]
 codeBase = file(bpipe.Config.config.script).parentFile.absolutePath
 load codeBase+"/tools.groovy"
-load codeBase+"/trinotate_stages/download_blastdb.groovy"
 
+//output directory
+blastdb_dir="blastdb"
 
-run {blastdb_download}
+download_uniprot = {
+    output.dir=blastdb_dir
+  {
+      exec """ 
+      wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_sprot_plants.dat.gz;
+      gunzip uniprot_sprot_plants.dat.gz
+   """ }
+}
+
+dat2fasta = {
+    output.dir=blastdb_dir
+   produce("uniprot_sprot_plants.fasta"){ 
+    exec "$python $dat2fasta > $output.fasta"
+   }
+}
+
+makeblastdb = {
+    output.dir=blastdb_dir
+   { 
+    exec "$makeblastdb -in $input.fasta -dbtype prot"
+   }
+}
+
+run {
+  download_uniprot + dat2fasta + makeblastdb
+}
